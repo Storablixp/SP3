8099c5abf6ef651684e4919763c4d6a1201b57f1
Storablixp <118421013+Storablixp@users.noreply.github.com>
2025-04-30 14:32:50 +0200

    TileRemover.

---
+++ Assets/Scripts/TileRemover.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileRemover : MonoBehaviour
+{
+    public Tilemap tilemap;
+    [SerializeField] private WorldTile hollowTile;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int cellPosition = tilemap.WorldToCell(worldPosition);
+
+            if (tilemap.HasTile(cellPosition))
+            {
+                WorldTile currentTile = (tilemap.GetTile(cellPosition)) as WorldTile;
+
+                if (currentTile.ColliderType != Tile.ColliderType.None)
+                {
+                    tilemap.SetTile(cellPosition, hollowTile);
+                    tilemap.SetColor(cellPosition, hollowTile.Color);
+                }
+            }
+        }
+    }
+}
