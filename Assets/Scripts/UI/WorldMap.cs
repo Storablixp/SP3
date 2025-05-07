3587d1f4d9aba5da899ca8af91546354ad6b0623
Storablixp <118421013+Storablixp@users.noreply.github.com>
2025-05-07 16:00:40 +0200

    World Map (Not Done).

---
+++ Assets/Scripts/UI/WorldMap.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldMap : MonoBehaviour
+{
+    [SerializeField] private RawImage rawImage;
+    [SerializeField] private Image playerMarker;
+    [SerializeField] private Transform playerTrans;
+    [SerializeField] private Vector2 worldSize;
+
+    private void Update()
+    {
+        
+    }
+}
