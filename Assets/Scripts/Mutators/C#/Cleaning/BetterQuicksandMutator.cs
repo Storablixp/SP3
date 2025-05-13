517cd818ba21b55339f8241321f46df9567b68f0
Storablixp <118421013+Storablixp@users.noreply.github.com>
2025-05-13 15:46:24 +0200

    Better Quicksand.

--- Assets/Scripts/Mutators/C#/Cleaning/BetterQuicksandMutator.cs
+++ Assets/Scripts/Mutators/C#/Cleaning/BetterQuicksandMutator.cs
@@ -5,28 +5,75 @@ using System.Collections;
 public class BetterQuicksandMutator : WorldMutatorSO
 {
     [Header("Pixels")]
+    [SerializeField] private PixelSO airPixel;
     [SerializeField] private PixelSO sandPixel;
     [SerializeField] private PixelSO quicksandPixel;
 
     public override IEnumerator ApplyMutator(Vector2Int worldSize)
     {
         PixelInstance[,] pixels = worldGenerator.RetrievePixels();
+        MoveUp(worldSize, pixels);
+        RemoveLines(worldSize, pixels);
 
+        yield return null;
+    }
+
+    private void MoveUp(Vector2Int worldSize, PixelInstance[,] pixels)
+    {
         for (int arrayX = 0; arrayX < worldSize.x; arrayX++)
         {
             for (int arrayY = startY; arrayY >= endY; arrayY--)
             {
                 PixelInstance pixel = pixels[arrayX, arrayY];
-                if (pixel.Pixel != quicksandPixel) continue;
+                if (pixel.Pixel != quicksandPixel ||
+                    GlobalNeighborCheckFucntions.SimpleCheck(arrayX, arrayY, Vector2Int.up, worldGenerator, airPixel)) continue;
 
                 if (GlobalNeighborCheckFucntions.SimpleCheck(arrayX, arrayY, Vector2Int.up, worldGenerator, sandPixel))
                 {
-                    worldGenerator.ChangePixel(arrayX, arrayY + 1, quicksandPixel);
-                    worldGenerator.ChangePixel(arrayX, arrayY, sandPixel);
+                    int i = 0;
+                    do
+                    {
+                        worldGenerator.ChangePixel(arrayX, arrayY + 1 + i, quicksandPixel);
+                        worldGenerator.ChangePixel(arrayX, arrayY + i, sandPixel);
+                        i++;
+                    }
+
+
+                    while (GlobalNeighborCheckFucntions.SimpleCheck(arrayX, arrayY + i, Vector2Int.up, worldGenerator, sandPixel));
+                }
+                else
+                {
+                    PixelSO pixelBeneath = pixels[arrayX, arrayY + 1].Pixel;
+                    worldGenerator.ChangePixel(arrayX, arrayY, pixelBeneath);
                 }
             }
         }
-
-        yield return null;
     }
+    private void RemoveLines(Vector2Int worldSize, PixelInstance[,] pixels)
+    {
+        for (int arrayX = 0; arrayX < worldSize.x; arrayX++)
+        {
+            for (int arrayY = startY; arrayY >= endY; arrayY--)
+            {
+                PixelInstance pixel = pixels[arrayX, arrayY];
+                if (pixel.Pixel == quicksandPixel)
+                {
+                    if (!GlobalNeighborCheckFucntions.MooreCheck(arrayX, arrayY, worldGenerator, 1, quicksandPixel, 2))
+                    {
+                        PixelSO pixelBeneath = pixels[arrayX - 1, arrayY].Pixel;
+                        worldGenerator.ChangePixel(arrayX, arrayY, pixelBeneath);
+                    }
+                }
+                else if (pixel.Pixel == sandPixel)
+                {
+                    if (!GlobalNeighborCheckFucntions.MooreCheck(arrayX, arrayY, worldGenerator, 1, sandPixel, 2))
+                    {
+                        PixelSO pixelBeneath = pixels[arrayX - 1, arrayY].Pixel;
+                        worldGenerator.ChangePixel(arrayX, arrayY, pixelBeneath);
+                    }
+                }
+            }
+        }
+    }
+
 }
