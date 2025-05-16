6e4907ebda2e4538517efc8721554f3ce6feadba
Storablixp <118421013+Storablixp@users.noreply.github.com>
2025-05-16 11:38:37 +0200

    Update.

---
+++ Assets/Scripts/Mutators/C#/Cave System/FluidMutator.cs
@@ -0,0 +1,132 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "Fluid Mutator", menuName = "Scriptable Objects/World Mutator/Cave System/Fluid")]
+public class FluidMutator : WorldMutatorSO
+{
+    [Header("Other Pixels")]
+    [SerializeField] private PixelSO AirPixel;
+    [SerializeField] private PixelSO HollowPixel;
+
+    [Header("Fluid Pixels")]
+    [SerializeField] private PixelSO IcePixel;
+    [SerializeField] private PixelSO LavaPixel;
+    [SerializeField] private PixelSO QuicksandPixel;
+    [SerializeField] private PixelSO WaterPixel;
+
+
+    public override IEnumerator ApplyMutator(Vector2Int worldSize)
+    {
+        PixelInstance[,] pixels = worldGenerator.RetrievePixels();
+
+        for (int arrayX = 0; arrayX < worldSize.x; arrayX++)
+        {
+            for (int arrayY = startY; arrayY >= endY; arrayY--)
+            {
+                PixelInstance pixelInstance = pixels[arrayX, arrayY];
+                if (pixelInstance.Pixel == AirPixel) continue;
+
+                if (pixelInstance.Depth == 1)
+                {
+                    if (pixelInstance.Wetness >= 1)
+                    {
+                        //if (pixelInstance.Temperature >= 2)
+                        //{
+                        //    if (pixelInstance.Wetness >= 2)
+                        //    {
+                        //        worldGenerator.ChangePixel(arrayX, arrayY, LavaPixel);
+                        //    }
+                        //}
+                        //else if (pixelInstance.Temperature == 1)
+                        //{
+                        //    if (pixelInstance.Wetness >= 2)
+                        //    {
+                        //        worldGenerator.ChangePixel(arrayX, arrayY, QuicksandPixel);
+                        //    }
+                        //}
+                        //else if (pixelInstance.Temperature <= -1)
+                        //{
+                        //    worldGenerator.ChangePixel(arrayX, arrayY, IcePixel);
+                        //}
+                        //else worldGenerator.ChangePixel(arrayX, arrayY, WaterPixel);
+
+                        if (pixelInstance.Temperature == 0)
+                        {
+                            if (!GlobalNeighborCheckFucntions.SimpleCheck(arrayX, arrayY, Vector2Int.down, worldGenerator, HollowPixel))
+                            {
+                                worldGenerator.ChangePixel(arrayX, arrayY, WaterPixel);
+
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+        //MoveUp(worldSize, pixels);
+        yield return null;
+    }
+
+
+    //private void MoveUp(Vector2Int worldSize, PixelInstance[,] pixels)
+    //{
+    //    for (int arrayX = 0; arrayX < worldSize.x; arrayX++)
+    //    {
+    //        for (int arrayY = startY; arrayY >= endY; arrayY--)
+    //        {
+    //            PixelInstance pixel = pixels[arrayX, arrayY];
+
+    //            List<PixelPair> pair = new List<PixelPair>();
+    //            pair.Add(new PixelPair
+    //            {
+    //                liquidPixel = QuicksandPixel,
+    //                terrainPixel = SandPixel
+    //            });
+    //            //pair.Add(new PixelPair
+    //            //{
+    //            //    liquidPixel = WaterPixel,
+    //            //    terrainPixel = DirtPixel
+    //            //});
+    //            pair.Add(new PixelPair
+    //            {
+    //                liquidPixel = IcePixel,
+    //                terrainPixel = SnowPixel
+    //            });
+
+    //            for (int i = 0; i < pair.Count; i++)
+    //            {
+
+    //                if (pixel.Pixel != pair[i].liquidPixel ||
+    //               GlobalNeighborCheckFucntions.SimpleCheck(arrayX, arrayY, Vector2Int.up, worldGenerator, AirPixel)) continue;
+
+    //                if (GlobalNeighborCheckFucntions.SimpleCheck(arrayX, arrayY, Vector2Int.up, worldGenerator, pair[i].terrainPixel))
+    //                {
+    //                    int y = 0;
+    //                    do
+    //                    {
+    //                        worldGenerator.ChangePixel(arrayX, arrayY + 1 + y, pair[i].liquidPixel);
+    //                        worldGenerator.ChangePixel(arrayX, arrayY + y, pair[i].terrainPixel);
+    //                        y++;
+    //                    }
+
+
+    //                    while (GlobalNeighborCheckFucntions.SimpleCheck(arrayX, arrayY + y, Vector2Int.up, worldGenerator, pair[i].terrainPixel));
+    //                }
+    //                else
+    //                {
+    //                    PixelSO pixelBeneath = pixels[arrayX, arrayY + 1].Pixel;
+    //                    worldGenerator.ChangePixel(arrayX, arrayY, pixelBeneath);
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
+
+    private class PixelPair
+    {
+        public PixelSO liquidPixel;
+        public PixelSO terrainPixel;
+    }
+}
