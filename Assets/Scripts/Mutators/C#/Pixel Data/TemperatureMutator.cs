88730b70ff3e3604aa5ca00d9a6287fafc443b00
Storablixp <118421013+Storablixp@users.noreply.github.com>
2025-05-19 08:13:02 +0200

    Tmeperature List.

--- Assets/Scripts/Mutators/C#/Pixel Data/TemperatureMutator.cs
+++ Assets/Scripts/Mutators/C#/Pixel Data/TemperatureMutator.cs
@@ -6,85 +6,11 @@ using UnityEngine.UIElements;
 [CreateAssetMenu(fileName = "Temperature Mutator", menuName = "Scriptable Objects/World Mutator/Pixel Data/Temperature")]
 public class TemperatureMutator : WorldMutatorSO
 {
-    [SerializeField] private List<int> temperatures = new();
-    [SerializeField, Min(16)] private int temperatureAreas;
     [Header("Settings")]
+    [SerializeField] private List<int> temperatures = new();
     [SerializeField] private Perlin2DSettings noiseSettings;
     [SerializeField, Range(0, 100)] private int heightVariationStrength = 50;
-    //[SerializeField] private List<int> temperatureList = new();
     private int startIndex;
-
-    public override void SetUp(WorldGenerator worldGenerator, Vector2Int worldSize)
-    {
-        base.SetUp(worldGenerator, worldSize);
-        temperatures.Clear();
-
-        //Fortsätt här.
-        while (true)
-        {
-            int currentValue = 0;
-            temperatures.Clear();
-
-            for (int i = 0; i < temperatureAreas; ++i)
-            {
-                if (i < 3)
-                {
-                    temperatures.Add(0);
-                }
-                else if (i >= temperatureAreas - 3)
-                {
-                    temperatures.Add(0);
-                }
-                else if (i == temperatureAreas - 4)
-                {
-                    if (temperatures[temperatureAreas - 5] == 2)
-                    {
-                        temperatures.Add(1);
-                    }
-                    else if (temperatures[temperatureAreas - 5] == -2)
-                    {
-                        temperatures.Add(-1);
-                    }
-                    else temperatures.Add(0);
-                }
-
-                else
-                {
-                    int nr = Random.Range(1, 11);
-                    int valueToAdd;
-                    if (nr > 6)
-                    {
-                        valueToAdd = 0;
-                    }
-                    else if (nr > 3 && nr <= 6)
-                    {
-                        valueToAdd = 1;
-                    }
-                    else
-                    {
-                        valueToAdd = -1;
-                    }
-
-                    if (currentValue + valueToAdd > 2)
-                    {
-                        currentValue = 1;
-                    }
-                    else if (currentValue + valueToAdd < -2)
-                    {
-                        currentValue = -1;
-                    }
-                    else
-                    {
-                        currentValue += valueToAdd;
-                    }
-
-                    temperatures.Add(currentValue);
-                }
-            }
-
-            if (temperatures.Contains(2) && temperatures.Contains(-2)) break;
-        }
-    }
 
     public override IEnumerator ApplyMutator(Vector2Int worldSize)
     {
@@ -134,12 +60,4 @@ public class TemperatureMutator : WorldMutatorSO
         }
         yield return null;
     }
-
-    //[System.Serializable]
-    //public struct TemperatureArea
-    //{
-    //    public string Name;
-    //    [Range(32, 256)] public int MinLength;
-    //    [Range(32, 256)] public int MaxLength;
-    //}
 }
